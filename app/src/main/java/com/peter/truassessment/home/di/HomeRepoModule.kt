92361271ac54f803f0ef254338617ac6fc9eb082
Peter <peter.atef@coformatique.com>
2025-05-15 00:59:55 +0300

    chore: rename HomeModule to HomeRepoModule

--- app/src/main/java/com/peter/truassessment/home/di/HomeRepoModule.kt
+++ app/src/main/java/com/peter/truassessment/home/di/HomeRepoModule.kt
@@ -1,19 +1,16 @@
 package com.peter.truassessment.home.di
 
-import com.peter.truassessment.home.data.datasources.ArticleMockDataSource
 import com.peter.truassessment.home.data.repo.ArticleRepoImpl
 import com.peter.truassessment.home.domain.repo.ArticleRepo
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class HomeModule {
-
+abstract class HomeRepoModule {
     @Binds
     @Singleton
     abstract fun bindArticlesRepo(
